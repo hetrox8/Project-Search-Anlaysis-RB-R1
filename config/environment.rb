597cd2d3a2fa4526cff1201948b0a6905198e148
suleiman gacheru <dretrevor8@gmail.com>
2024-05-26 11:52:16 +0300

    fix deployment configs

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative "application"
-
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.secret_key_base = ENV['RAILS_MASTER_KEY']
