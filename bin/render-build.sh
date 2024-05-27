a681ad62eee814412a90adc2adc88916da64aea6
suleiman gacheru <dretrevor8@gmail.com>
2024-05-27 23:06:44 +0300

    update render config files to include migration commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,6 @@ set -o errexit
 chmod +x ./bin/rails   # Ensure rails script has execute permission
 
 bundle install
+bundle exec rails db:migrate
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
